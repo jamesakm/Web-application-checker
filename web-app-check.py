7bdc279e6a37ea259a316f5fdb5fe00e7295ef6d
jamesakm <jamesakm007@gmail.com>
2018-12-19 14:18:59 +0530

    Update web-app-check.py

--- web-app-check.py
+++ web-app-check.py
@@ -1,12 +1,22 @@
+
 #!/usr/bin/python3.6
 import subprocess
 import argparse
+
+# Fun to check the applications
+
+def web_app_check(domain):
+  print(subprocess.call('/usr/bin/wad -u' +  domain, shell=True))
+  return
+
 ap = argparse.ArgumentParser(prog="WebSoftware Checker")
 ap.add_argument("--url", "-u", help="List the WebSoftwares of your domain")
 args = ap.parse_args()
 host = args.url
 http = 'http://'
 trail = '/'
-domain = http + host + trail
-print("\n\n\n\t\t\tWEB APPLICATIONS DETECTED FOR",host.upper(),"\n\t\t\t","#"*37, "\n\n")
-print(subprocess.call('/usr/bin/wad -u' +  domain, shell=True))
+if host.startswith('http'):
+ web_app_check(host)
+else:
+ domain = http + host + trail
+ web_app_check(domain)
